386c32a81dd819f9e241d1e86d50b1b0690ac49f
Rinaldo Di Giorgio <rinaldo@digiorgio.com>
2014-11-12 19:28:54 -0800

    Transfer UAT test to demonstrate an issue and updates to Transfer UAT test.

--- src/uat/TransferUATVerboseTest.java
+++ src/uat/TransferUATVerboseTest.java
@@ -4,6 +4,7 @@ import org.concordion.integration.junit3.ConcordionTestCase;
 
 import database.Database;
 import banksystem.Account;
+import banksystem.AccountData;
 import banksystem.AccountOwner;
 import banksystem.Transfer;
 
@@ -11,7 +12,6 @@ import banksystem.Transfer;
 public class TransferUATVerboseTest extends ConcordionTestCase {
 
 	Database database = Database.getInstance();
-
 
 	public void setUp() throws Exception {
 		database.setFileName("test.dat");
@@ -29,6 +29,8 @@ public class TransferUATVerboseTest extends ConcordionTestCase {
 
 		AccountOwner accountOwner = new AccountOwner(name, password);
 		accountOwner.put();
+		System.out.println ("AccountOwner:" +  accountOwner );
+
 		return ( accountOwner );
 	}
 
@@ -36,24 +38,43 @@ public class TransferUATVerboseTest extends ConcordionTestCase {
 		database.setIndex(requestedId);
 		Account account = new Account(ownerId, accountType, balance);
 		account.put();
+		System.out.println ("Account:" +  account );
 		return (account);
 	}
 
 	public TransferUAT makeTransfer(String password, String tryPassword, String fromBalance, String toBalance, String transferAmount,
 			String givenOwnerId, String fromAccountId, String toAccountId) {
+		database.setIndex(givenOwnerId);		
 
 		Transfer transfer = new Transfer(givenOwnerId, fromAccountId, toAccountId, transferAmount);
-		String result = transfer.validate(tryPassword);
-		TransferUAT transferUAT = new TransferUAT();
-		if ( result.equals("valid") ) {
-
+		System.out.println ( "givenOwnerId" + givenOwnerId );
+		System.out.println ( "fromAccountId" +  fromAccountId );
+		System.out.println ( "toAccountId" +  toAccountId );
+		System.out.println ( "transferAmount" +  transferAmount );
 		
+		String result = transfer.validate(tryPassword);
+		System.out.println ( "Result of transfer:" + result );
+		TransferUAT transferUAT = new TransferUAT();
+		
+		if ( result.equals("valid") ) {
+			result = transfer.transfer(tryPassword);
+			System.out.println ( "Result of transfer:" + transfer );
+			transfer.put();
 			transferUAT.fromOwnerId = givenOwnerId;
-			transferUAT.toAccountId = transfer.getToAccountId();
-			transferUAT.toAccountBalance = transfer.getTransferAmount();
+			transferUAT.toAccountId = toAccountId;
+			transferUAT.fromAccountId = fromAccountId;
 			transferUAT.toOwnerId = givenOwnerId;
-			transferUAT.setStatus(result);
+
+			AccountData toAccountData = (AccountData)Database.get(toAccountId);
+			transferUAT.toAccountBalance = toAccountData.balance;
+
+			AccountData fromAccountData = (AccountData)Database.get(fromAccountId);
+			transferUAT.fromAccountBalance = fromAccountData.balance;
+
+			transferUAT.transferAmount = transferAmount;
 		}
+		transferUAT.setStatus(result);
+		System.out.println ( transferUAT );
 		return (transferUAT);
 
 	}
